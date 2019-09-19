94a7178de789f16154be7db9d5565493bcaac6ee
mderrier <maximilien.derrier@gmail.com>
2019-09-19 22:19:43 +0200

    fix(releases): node version

---
+++ ci/prebuild-publish.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if %1 == 12 (
+  if %APPVEYOR_REPO_BRANCH% == master (
+    if %GITHUB_TOKEN% neq "" (
+      npm run prebuild-node -- -u %GITHUB_TOKEN%
+      npm run prebuild-node-ia32 -- -u %GITHUB_TOKEN%
+      npm run prebuild-electron -- -u %GITHUB_TOKEN%
+      npm run prebuild-electron-ia32 -- -u %GITHUB_TOKEN%
+      npm run prebuild-electron -- -u %GITHUB_TOKEN%
+    )
+  )
+)
