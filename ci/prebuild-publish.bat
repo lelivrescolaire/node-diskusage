9efaf536dbe5113e40baba68ddabb123b915094f
mderrier <mderrier@users.noreply.github.com>
2023-03-06 17:27:49 +0100

    Update prebuild-publish.bat

--- ci/prebuild-publish.bat
+++ ci/prebuild-publish.bat
@@ -2,6 +2,7 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
+    python3 -m pip install gyp-next
     npm run prebuild-node -- -u %GITHUB_TOKEN%
     npm run prebuild-node-ia32 -- -u %GITHUB_TOKEN%
     npm run prebuild-electron -- -u %GITHUB_TOKEN%
